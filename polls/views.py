ae7a125f792e2e1a783a6c4063a3f0e4f7736ef8
HC <hakan.cuzdan@yandex.com>
2019-11-27 15:43:22 +0200

    mysite : polls app : 4 dummy views (index, detail, results, vote) added

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
