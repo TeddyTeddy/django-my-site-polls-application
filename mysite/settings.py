736bd6c29c2736d22f1917fd92c4152a2baa60c8
HC <hakan.cuzdan@yandex.com>
2019-11-27 12:42:27 +0200

    mysite: postgresql is configured

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pollsdatabase',
+        'USER': 'hakan',
+        'PASSWORD': 'h1a2k3a4',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
